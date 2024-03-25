6e16ad5ae970e9f5e0d8ed4f8b3686334a38cc42
freyes <fran@codesai.com>
2024-03-25 23:24:51 +0000

    add Course Duration

---
+++ CourseDuration/CourseDuration.Tests/CourseTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace CourseDuration.Tests;
+
+public class CourseTest
+{
+    private Course _course;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _course = new Course("macramé");
+    }
+    
+    [Test]
+    public void Identifies_Short_Courses()
+    {
+       _course.Start();
+       _course.End();
+       
+       Assert.That(_course.IsShort, Is.True);
+    }
+
+    [Test]
+    [Ignore("pending")]
+    public void Identifies_Long_Courses()
+    {
+    }
+    
+    [Test]
+    [Ignore("pending")]
+    public void Knows_The_Course_Title()
+    {
+    }
+    
+}
---
+++ CourseDuration/CourseDuration/Course.cs
@@ -0,0 +1,39 @@
+﻿namespace CourseDuration;
+
+public class Course
+{
+    private readonly String _name;
+    private int _startTime;
+    private int _durationInMinutes;
+
+    public Course(string name) {
+        _name = name;
+        _durationInMinutes = 0;
+    }
+    
+    public void Start() {
+        _startTime = DateTime.Now.Millisecond;
+    }
+    
+    public void End() {
+        var endTime = DateTime.Now.Millisecond;
+        _durationInMinutes = (endTime - _startTime) / (1000 * 60);
+    }
+
+    public bool IsShort() {
+        var tenMinutes = 10 * 60;
+        return _durationInMinutes < tenMinutes;
+    }
+    
+    public bool IsLong() {
+        return !IsShort();
+    }
+
+    public String GetTitle() {
+        return $"{_name} course in {GetCollege()} college";
+    }
+
+    private string GetCollege() {
+        return Environment.GetEnvironmentVariable("COLLEGE") ?? "not found";
+    }
+}
