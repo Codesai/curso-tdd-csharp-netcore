aab529f92cad650704acaf51efffb920070bf581
freyes <fran@codesai.com>
2024-03-27 16:15:56 +0000

    Add Morning Routine Kata

---
+++ awkward_dependencies_examples/morning-routine/MorningRoutine.Tests/MyMorningRoutingTest.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace MorningRoutine.Tests;
+
+public class MyMorningRoutingTest
+{
+   [Test]
+   public void Fix_Me()
+   {
+      Assert.That(false, Is.True);
+   }
+}
---
+++ awkward_dependencies_examples/morning-routine/MorningRoutine/MyMorningRouting.cs
@@ -0,0 +1,19 @@
+﻿namespace MorningRoutine;
+
+public class MyMorningRouting
+{
+    public void WhatShouldIDoNow()
+    {
+        var now = DateTime.Now;
+        int currentHour = now.Hour;
+        if (currentHour == 6) {
+            Console.WriteLine("Do exercise");
+        } else if (currentHour == 7) {
+            Console.WriteLine("Read and study");
+        } else if (currentHour == 8) {
+            Console.WriteLine("Have breakfast");
+        } else {
+            Console.WriteLine("No activity");
+        }
+    }
+}
