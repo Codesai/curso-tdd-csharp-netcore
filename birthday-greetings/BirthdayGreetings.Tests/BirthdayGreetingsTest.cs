25d3393fae7b86962d1ab44027468d72e8ff921d
trikitrok <trikitrok@gmail.com>
2024-04-07 21:16:46 +0200

    adding new kata

---
+++ birthday-greetings/BirthdayGreetings.Tests/BirthdayGreetingsTest.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace BirthdayGreetings.Tests
+{
+    public class BirthdayGreetingsTest
+    {
+        [Test]
+        public void Fix_Me_And_Rename_Me()
+        {
+            Assert.That(false, Is.True);
+        }
+    }
+}
