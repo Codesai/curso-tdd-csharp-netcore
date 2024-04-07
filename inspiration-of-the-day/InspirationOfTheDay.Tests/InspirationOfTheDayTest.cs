496affeaef0837f5f32cd7313b1814e42c019fb1
trikitrok <trikitrok@gmail.com>
2024-04-07 21:40:46 +0200

    adding new kata

---
+++ inspiration-of-the-day/InspirationOfTheDay.Tests/InspirationOfTheDayTest.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace InspirationOfTheDay.Tests
+{
+    public class InspirationOfTheDayTest
+    {
+        [Test]
+        public void Fix_Me_And_Rename_Me()
+        {
+            Assert.That(false, Is.True);
+        }
+    }
+}
