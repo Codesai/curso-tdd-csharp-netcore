9a075fda7b6365b3b69ec5ed8b7b2db171583fe4
trikitrok <trikitrok@gmail.com>
2024-04-07 21:05:11 +0200

    adding new kata

---
+++ ohce/Ohce.Tests/OhceTest.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Ohce.Tests
+{
+    public class OhceTest
+    {
+        [Test]
+        public void Fix_Me_And_Rename_Me()
+        {
+            Assert.That(false, Is.True);
+        }
+    }
+}
