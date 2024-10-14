93563816c059156591414ba1f9539ed106a917cc
Jaume Borras Rossello <jaume.borras@toolfactory.net>
2024-10-14 15:35:19 +0200

    feature: completed FizzBuzz by TDD

--- fizz-buzz/FizzBuzz.Tests/FizzBuzzTest.cs
+++ fizz-buzz/FizzBuzz.Tests/FizzBuzzTest.cs
@@ -1,13 +1,66 @@
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace FizzBuzz.Tests
 {
     public class FizzBuzzTest
     {
-        [Test]
-        public void My_First_Test()
+        private FizzBuzz _fizzBuzz;
+
+        [SetUp]
+        public void SetUp()
         {
-            Assert.That(false, Is.True);
+            _fizzBuzz = new FizzBuzz();
         }
+
+        [Test]
+        public void Return_Number()
+        {
+            var result = _fizzBuzz.GetElements();
+
+            Assert.That(GetResultElement(1, result), Is.EqualTo("1"));
+            Assert.That(GetResultElement(98, result), Is.EqualTo("98"));
+        }
+
+        [Test]
+        public void Return_100_Elements_List()
+        {
+            var result = _fizzBuzz.GetElements();
+
+            Assert.That(result.Count, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void Return_Fizz_WhenIsDivisibleByThree()
+        {
+            var result = _fizzBuzz.GetElements();
+
+            Assert.That(GetResultElement(3, result), Is.EqualTo("Fizz"));
+            Assert.That(GetResultElement(33, result), Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void Return_Buzz_WhenIsDivisibleByFive()
+        {
+            var result = _fizzBuzz.GetElements();
+
+            Assert.That(GetResultElement(5, result), Is.EqualTo("Buzz"));
+            Assert.That(GetResultElement(25, result), Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void Return_FizzBuzz_WhenIsDivisibleBy_ThreeAndFive()
+        {
+            var result = _fizzBuzz.GetElements();
+
+            Assert.That(GetResultElement(15, result), Is.EqualTo("FizzBuzz"));
+            Assert.That(GetResultElement(60, result), Is.EqualTo("FizzBuzz"));
+        }
+
+        private static string GetResultElement(int number, IList<string> elements)
+        {
+            return elements[number - 1];
+        }
+
     }
 }
--- fizz-buzz/FizzBuzz/FizzBuzz.cs
+++ fizz-buzz/FizzBuzz/FizzBuzz.cs
@@ -1,8 +1,37 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace FizzBuzz
 {
     public class FizzBuzz
     {
+        public IList<string> GetElements()
+        {
+            IList<string> elements = new List<string>();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                bool isFizz = i % 3 == 0;
+                bool isBuzz = i % 5 == 0;
+
+                if (isFizz && isBuzz)
+                {
+                    elements.Add("FizzBuzz");
+                }
+                else if (isFizz)
+                {
+                    elements.Add("Fizz");
+                }
+                else if (isBuzz)
+                {
+                    elements.Add("Buzz");
+                }
+                else
+                {
+                    elements.Add(i.ToString());
+                }
+            }
+
+            return elements;
+        }
     }
 }
